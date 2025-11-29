135ee8c703db642418b59f704f7b1b5ec5404844
Aluno <aluno@facens.br>
2025-11-28 21:20:01 -0300

    first commit

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,31 @@
+package  calculadora;
+
+public class Calculadora {
+    
+    public int r  = 0;
+
+    //Calculadora
+    public int calc(int a, int b, String op){
+
+        if (op.equals("+")){
+            r = a + b;
+        } else if (op.equals("-")) {
+            r = a - b;
+        }else if (op.equals("*")) {
+            r = a * b;
+        }else if (op.equals("/")){
+            if(b != 0){
+            r = a / b;
+        }else{
+            System.out.println("Erro: divisao por zero, operação irregular");
+            r = 0;
+        }
+    } else {
+        System.out.println("Operação invalida");
+        r = 0;
+    }
+
+    System.out.println("Resultado = " + r);
+    return r;
+}
+}
